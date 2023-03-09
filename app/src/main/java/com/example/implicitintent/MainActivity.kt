85e8a57e42b4832ceaca03c6c0f74e58efed426b
sktiwari001 <shubhamsandilya99@gmail.com>
2023-03-09 21:33:59 +0530

    I have added the files of the project based on ImplicitIntent

---
+++ app/src/main/java/com/example/implicitintent/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.implicitintent
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import androidx.cardview.widget.CardView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val webButton=findViewById<CardView>(R.id.cardWeb)
+        val cameraButton=findViewById<CardView>(R.id.cardCamera)
+
+        webButton.setOnClickListener{
+            val intent=Intent(Intent.ACTION_VIEW)
+            intent.data= Uri.parse("https://www.linkedin.com/in/shubham-kumar-384025229/")
+            startActivity(intent)
+        }
+
+        cameraButton.setOnClickListener{
+            val intent=Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivity(intent)
+        }
+    }
+}
